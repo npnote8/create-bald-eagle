73e5bcecd1b51ca0562822da589cf640e31f50f7
Alina Mats <npnote8@gmail.com>
2022-10-22 14:15:18 -0700

    Moved todoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-let todoList = [];
-todoList.push({ id: 1, title: "Complete assignment" });
-todoList.push({ id: 2, title: "Complete reading the textbook" });
-todoList.push({ id: 3, title: "Compete watching tutorials" });
-
 function App() {
+  let todoList = [];
+  todoList.push({ id: 1, title: "Complete assignment" });
+  todoList.push({ id: 2, title: "Complete reading the textbook" });
+  todoList.push({ id: 3, title: "Compete watching tutorials" });
+
   return (
     <div>
       <h1>Todo List</h1>
