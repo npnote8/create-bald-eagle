7fd989a2a33e2f13720e26fb3f889cfb9fd71473
Alina Mats <npnote8@gmail.com>
2022-10-25 17:31:14 -0700

    TodoList and AddToDoForm Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  let todoList = [];
-  todoList.push({ id: 1, title: "Complete assignment" });
-  todoList.push({ id: 2, title: "Complete reading the textbook" });
-  todoList.push({ id: 3, title: "Compete watching tutorials" });
-
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <TodoList />
+      <AddTodoForm />
     </div>
   );
 }
