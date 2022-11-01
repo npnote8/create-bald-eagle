9b7ad63fa77d3b63908c47f0c305e8f23371479d
Alina Mats <npnote8@gmail.com>
2022-11-01 15:36:34 -0700

    Stored 'New Todo' in React State

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,16 @@
 import React from "react";
+import TodoListItem from "./TodoListItem";
 
 const TodoList = () => {
   const todoList = [
     { id: 1, title: "Complete assignment" },
     { id: 2, title: "Complete reading the textbook" },
-    { id: 3, title: "Compete watching tutorials" },
+    { id: 3, title: "Complete watching tutorials" },
   ];
   return (
     <ul>
       {todoList.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <TodoListItem key={item.id} item={item} />
       ))}
     </ul>
   );
